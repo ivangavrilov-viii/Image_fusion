01c7d90342b137bc7b5e9bcb7ce03cd0b8e5f6a7
gavrilovivan2001@gmail.com <34HnoP-33GavrI+>
2023-03-13 17:11:32 +0300

    13/03/2023

--- request.py
+++ request.py
@@ -1,13 +1,46 @@
 import requests
 import urllib
+from requests.auth import HTTPDigestAuth
+
+def get_snap():
+    user = 'admin'
+    password = 'Qwerty404'
+    # link = 'http://<IP>/onvifsnapshot/media_service/snapshot?channel=1&subtype=0'
+    # link = 'http://192.168.1.40:80/streaming/channels/102/httpPreview'
+    session = requests.Session()
+    print(session)
+
+    session.auth = HTTPDigestAuth(user, password)
+    photo = session.get('http://admin:Qwerty404@192.168.1.40:80/streaming/channels/102/httpPreview')
+
+    session.close()
+    if photo.ok:
+        print('OK')
+        f = open('./images/video/', 'wb')
+        f.write(photo.content)
+        f.close()
+    else:
+        print('error')
+
 
 while True:
+    get_snap()
+
+
+
+
+# while True:
+#     pass
     # res = requests.get(r'http://admin:Qwerty404@192.168.1.40/streaming/channels/202/httpPreview', stream=True)
     # res = requests.get(r'http://admin:Qwerty404@192.168.1.40:80/ISAPI/Streaming/channels/201/picture?snapShotImageType=JPEG')
     # res = requests.get(r'http://admin:Qwerty404@192.168.1.40:80/ISAPI/Streaming/channels/201/picture?snapShotImageType=JPEG')
     # res = requests.get(r'http://admin:Qwerty404@192.168.1.40/ISAPI/Streaming/channels/101/picture?snapShotImageType=JPEG')
-    res = requests.get(r'http://admin:Qwerty404@192.168.1.40:80/streaming/channels/102/httpPreview')
-    print(res)
+    # res = requests.get(r'http://admin:Qwerty404@192.168.1.40:80/streaming/channels/102/httpPreview')
+    # res = requests.get(r'https://192.168.1.40:80/login?admin=login&password=Qwerty404')
+    # print(res)
+
+
+
 
 
 # resource = urllib.urlretrieve(r'http://admin:Qwerty404@192.168.1.40:80/ISAPI/Streaming/channels/201/picture?snapShotImageType=JPEG', '...\img.jpg"')
--- vlc_player.py
+++ vlc_player.py
@@ -1,5 +1,21 @@
-import vlc
+import cv2 as cv
+import numpy as np
 
+visual_video = cv.VideoCapture("rtsp://admin:Qwerty404@192.168.1.40:554/Streaming/Channels/101")
+ir_video = cv.VideoCapture("rtsp://admin:Qwerty404@192.168.1.40:554/Streaming/Channels/201")
 
-my_video = vlc.MediaPlayer("")
+while True:
+    ret_visual, frame_visual = visual_video.read()
+    ret_ir, frame_ir = ir_video.read()
 
+    for i in range(len(frame_visual[0])):
+        print(i)
+        for j in range(len(frame_visual[1])):
+            print(f'Element i={i}, j={j}')
+            print(frame_visual[i][j][0])
+            frame_visual[i][j] = np.uint8(frame_visual[i][j])
+            print(frame_visual[i][j])
+    # print(len(frame[0]), len(frame[1])s)
+    cv.imshow('VIDEO_VD', frame_visual)
+    cv.imshow('VIDEO_IR', frame_ir)
+    cv.waitKey(1)
